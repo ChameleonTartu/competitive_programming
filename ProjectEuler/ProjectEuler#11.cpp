31e11b1d633a842a598d9bc2284a718feb09b146
Dmytro Chasovskyi <dmytro.chasovskyi@ut.ee>
2016-07-16 22:53:52 +0300

    Change code on the correct for problem #11

--- ProjectEuler/ProjectEuler#11.cpp
+++ ProjectEuler/ProjectEuler#11.cpp
@@ -1,42 +1,80 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 using namespace std;
 
-typedef unsigned long long ull;
-
-const int MAX = 1000001;
+typedef vector<int> vint;
+typedef vector<vint> vvint;
 
 #define endl '\n'
+#define max(a, b) a > b ? a : b
 
-int isPrime(int number, vector<ull>&prime) {
-    if(number == 2) return 1;
-    if(number % 2 == 0) return 0;
-    for(const int &i: prime) {
-        if(i*i > number) return 1;
-        if(number % i == 0) return 0;
+const int SIZE = 20, WINDOW = 3;
+
+vvint matrix(SIZE, vint(SIZE));
+
+
+int down(vvint &m) {
+    int result = 0;
+    for(int i = 0; i < (SIZE - WINDOW); ++i)
+        for(int j = 0; j < SIZE; ++j) {
+            int sum = 1;
+            for(int k = 0; k <= WINDOW; ++k)
+                sum *= m[i + k][j];
+            result = max(result, sum);
     }
-    return 1;
+    return result;
 }
 
-vector<ull> findPrimeSums(const int MAX) {
-    vector<ull> primes(MAX + 1, 0);
-    vector<ull> values;
-    for(int i = 2; i < MAX; ++i) {
-        primes[i] = primes[i - 1];
-        if(isPrime(i, values)) {
-            primes[i] += i;
-            values.push_back(i);
-        }
+int right(vvint &m) {
+    int result = 0;
+    for(int i = 0; i < SIZE; ++i)
+        for(int j = 0; j < (SIZE - WINDOW); ++j) {
+            int sum = 1;
+            for(int k = 0; k <= WINDOW; ++k)
+                sum *= m[i][j + k];
+            result = max(result, sum);
     }
-    return primes;
+    return result;
+}
+
+int diag(vvint &m) {
+    int result = 0;
+    for(int i = 0; i < (SIZE - WINDOW); ++i)
+        for(int j = 0; j < (SIZE - WINDOW); ++j) {
+            int sum = 1;
+            for(int k = 0; k <= WINDOW; ++k)
+                sum *= m[i + k][j + k];
+        result = max(result, sum);
+    }
+    return result;
+}
+
+int antidiag(vvint &m) {
+    int result = 0;
+    for(int i = 0; i < (SIZE - WINDOW); ++i) 
+        for(int j = WINDOW; j < SIZE; ++j) {
+            int sum = 1;
+            for(int k = 0; k <= WINDOW; ++k)
+                sum *= m[i + k][j - k];
+            result = max(result, sum);
+    }
+    return result;
+}
+
+
+int calculate(vvint &m) {
+    int result = 0;
+    result = max(result, down(m));
+    result = max(result, right(m));
+    result = max(result, diag(m));
+    result = max(result, antidiag(m));
+    return result;
 }
 
 int main() {
-    vector<ull> primes = findPrimeSums(MAX);
-    int tests; cin >> tests;
-    for(int i = 0; i < tests; ++i) {
-        int N; cin >> N;
-        cout << primes[N] << endl;
-    }
+    for(int i = 0; i < SIZE; ++i)
+        for(int j = 0; j < SIZE; ++j)
+            cin >> matrix[i][j];
+    cout << calculate(matrix) << endl;
     return 0;
 }
